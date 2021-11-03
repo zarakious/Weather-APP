98de7f0f490df4ceb383b931c0f5e06cd140a768
zaraki <ziiedkouki@gmail.com>
2021-11-03 18:47:21 +0100

    final tweaks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const api = {
+	key: "f9d49f5fc6bdd1725d9915488bd448d0",
+	base: "https://api.openweathermap.org/data/2.5/",
+};
+
+const App = () => {
+	const dateBuilder = (d) => {
+		let months = [
+			"januray",
+			"Febraury",
+			"March",
+			"April",
+			"May",
+			"Juin",
+			"July",
+			"August",
+			"September",
+			"Oktober",
+			"November",
+			"December",
+		];
+		let days = [
+			"Monday",
+			"Tuesday",
+			"Wednesday",
+			"Thursday",
+			"Friday",
+			"Saturday",
+			"Sunday",
+		];
+		let day = days[d.getDay()];
+		let date = d.getDate();
+		let month = months[d.getMonth()];
+		let year = d.getFullYear();
+		return `${day} ${date} ${month} ${year}`;
+	};
+	const [query, setQuery] = useState("");
+	const [weather, setWeather] = useState("");
+
+	const search = (evt) => {
+		if (evt.key === "Enter") {
+			fetch(`${api.key}weather?q=${query}&units=metric&APPID=${api.key}`)
+				.then((res) => res.json())
+				.then((result) => setWeather(result));
+		}
+	};
+	return (
+		<div className="app warm">
+			<main>
+				<div className="search-box">
+					<input type="text" className="search-bar" placeholder="search...." />
+				</div>
+
+				<div className="location-box">
+					<div className="location">Tunis, Tunisia</div>
+					<div className="date">{dateBuilder(new Date())}</div>
+				</div>
+				<div className="weather-box">
+					<div className="temp">15°c</div>
+					<div className="weather">Sunny</div>
+				</div>
+			</main>
+		</div>
+	);
+};
 
 export default App;
